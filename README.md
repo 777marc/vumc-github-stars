b4ff5a827510eadcf039fc777df9514e280ce098
777marc@gmail.com <777marc@gmail.com>
2023-01-26 19:43:46 -0500

    adding listing

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
+### prerequisite: composer, docker
+
 ### 1. From the project root, install dependancies by running: `$ composer install`
 
-### 2. Next, run `./vendor/bin/sail up`
+### 2. create a new .env file and copy contents of .env.example into it
 
-### 3. Next, run `./vendor/bin/sail php artisan migrate:fresh --seed` to created the database and seed the Github Stars table with initial dummy data.
+### 3. Next, run `./vendor/bin/sail up -d`
+
+### 4. Next, run `./vendor/bin/sail php artisan migrate:fresh --seed` to created the database and seed the Github Stars table with initial dummy data.
+
+### 5. Install client dependencies: `./vendor/bin/sail npm i`
+
+### 5. Build client: `./vendor/bin/sail npm run build`
+
+### 6. Run the client: `./vendor/bin/sail npm run dev`
--- resources/js/Pages/Stars.jsx
+++ resources/js/Pages/Stars.jsx
@@ -1,8 +1,34 @@
 import { Link, Head } from "@inertiajs/react";
 
 const NumberOfStars = (props) => {
-    return <p>{props.stars}</p>;
+    return (
+        <>
+            <h2>{props.repoName}</h2>
+            <p>stars: {props.stars}</p>
+        </>
+    );
 };
+
+const stars = [
+    {
+        repository_id: 123,
+        name: "my first repo",
+        url: "https://github.com/777marc/myrepo",
+        created_date: "2020-01-15",
+        last_publish_date: "2022-07-17",
+        description: "A really cool repo about things",
+        number_of_starts: 354,
+    },
+    {
+        repository_id: 124,
+        name: "my second repo",
+        url: "https://github.com/777marc/myrepo",
+        created_date: "2020-01-15",
+        last_publish_date: "2022-07-17",
+        description: "A really cool repo about things",
+        number_of_starts: 354,
+    },
+];
 
 export default function Stars(props) {
     return (
@@ -10,7 +36,13 @@ export default function Stars(props) {
             <Head title="Stars" />
             <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
                 <h1 className="text-white">Github Stars</h1>
-                <NumberOfStars stars={props.stars} />
+                {stars.forEach((star) => {
+                    console.log("star:", star);
+                    <NumberOfStars
+                        repoName={star.name}
+                        stars={star.number_of_starts}
+                    />;
+                })}
             </div>
         </>
     );
